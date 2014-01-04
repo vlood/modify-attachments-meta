b50de4f89a8e64a3f14cbc37fe80541fcfff9f8f
Vladimir Vassilev <vlood@shtrak.eu>
2014-01-04 15:54:59 +0200

    readme.txt updates and checking if functions exist

--- modify-attachments-meta.php
+++ modify-attachments-meta.php
@@ -1,12 +1,8 @@
 <?php
 /**
- * @package   Modify Attachments Meta
- * @author    Vladimir Vassilev <vlood.vassilev@gmail.com>
- * @license   GPL-2.0+
- * @link      http://shtrak.eu
  *
  * Plugin Name:       Modify Attachments Meta
- * Plugin URI:        http://shtrak.eu
+ * Plugin URI:        https://github.com/vlood/modify-attachments-meta
  * Description:       Allows modification of meta data of attachments, such as date fields (soon to add more, I guess)
  * Version:           0.1
  * Author:            vloo
@@ -31,22 +27,26 @@ add_action('plugins_loaded', 'mod_att_meta_init');
 /* Set attachment post date (thanks to http://wordpress.org/support/profile/herrvollbaer, posting a solution here: http://wordpress.org/support/topic/modify-date-of-attachments?replies=5#post-3070220) */
 
 // Add a custom field to an attachment in WordPress
-function attachment_date_edit($form_fields, $post) {
-    $form_fields['post_date']['label'] = __('Date and time', 'mod-att-meta');
-    $form_fields['post_date']['value'] = $post->post_date;
-    $form_fields['post_date']['helps'] = __('Modify the original upload date', 'mod-att-meta');
-    return $form_fields;
+if(!function_exists('attachment_date_edit')){
+	function attachment_date_edit($form_fields, $post) {
+	    $form_fields['post_date']['label'] = __('Date and time', 'mod-att-meta');
+	    $form_fields['post_date']['value'] = $post->post_date;
+	    $form_fields['post_date']['helps'] = __('Modify the original upload date', 'mod-att-meta');
+	    return $form_fields;
+	}
+	
+	add_filter( 'attachment_fields_to_edit', 'attachment_date_edit', 10, 2);
 }
 
 // save custom field to post_meta
-function attachment_date_save($post, $attachment) {
-	if(strtotime($attachment['post_date'])){ //is a valid date?
-		$post['post_date'] = $attachment['post_date'];
-    	return $post;	
-	} else {
-		// Do nothing silently. That's what you get when you don't give us valid input!
+if(!function_exists('attachment_date_save')){
+	function attachment_date_save($post, $attachment) {
+		if(strtotime($attachment['post_date'])){ //is a valid date?
+			$post['post_date'] = $attachment['post_date'];
+	    	return $post;	
+		} else {
+			// Do nothing silently. That's what you get when you don't give us valid input!
+		}
 	}
+	add_filter( 'attachment_fields_to_save', 'attachment_date_save', 10, 2);
 }
-
-add_filter( 'attachment_fields_to_edit', 'attachment_date_edit', 10, 2);
-add_filter( 'attachment_fields_to_save', 'attachment_date_save', 10, 2);
--- readme.txt
+++ readme.txt
@@ -19,6 +19,7 @@ If a user types invalid input, they should be warned. Currently nothing happens 
 * Changing file name of the attachment (without the extension, of course);
 * Control over which user roles can edit these fields;
 * Validation error messages should be displayed;
+* Use anonymous functions, instead of worrying what'll happen if a function is defined by someone else;
 
 As I'm lazy as hell, I'll need your help to think of new features to implement!
 
@@ -27,16 +28,16 @@ As I'm lazy as hell, I'll need your help to think of new features to implement!
 = From your WordPress dashboard =
 
 1. Visit 'Plugins > Add New'
-2. Search for 'Modify Attachments Meta'
-3. Activate Modify Attachments Meta from your Plugins page.
+1. Search for 'Modify Attachments Meta'
+1. Activate Modify Attachments Meta from your Plugins page.
 
 Now you can edit you attachments' date and time of upload. Other features will be coming soon.
 
 = From WordPress.org =
 
 1. Download 'Modify Attachments Meta'.
-2. Upload the 'modify-attachments-meta' directory to your '/wp-content/plugins/' directory, using your favorite method (ftp, sftp, scp, etc...)
-3. Activate it from your Plugins page.
+1. Upload the 'modify-attachments-meta' directory to your '/wp-content/plugins/' directory, using your favorite method (ftp, sftp, scp, etc...)
+1. Activate it from your Plugins page.
 
 == Frequently Asked Questions ==
 
@@ -52,28 +53,3 @@ Nope.
 = 0.1 =
 
 * Editing an attachment date and time record is now possible;
-
-== A brief Markdown Example ==
-
-Ordered list:
-
-1. Some feature
-1. Another feature
-1. Something else about the plugin
-
-Unordered list:
-
-* something
-* something else
-* third thing
-
-Here's a link to [WordPress](http://wordpress.org/ "Your favorite software") and one to [Markdown's Syntax Documentation][markdown syntax].
-Titles are optional, naturally.
-
-[markdown syntax]: http://daringfireball.net/projects/markdown/syntax
-            "Markdown is what the parser uses to process much of the readme file"
-
-Markdown uses email style notation for blockquotes and I've been told:
-> Asterisks for *emphasis*. Double it up  for **strong**.
-
-`<?php code(); // goes in backticks ?>`
