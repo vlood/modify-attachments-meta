5dad7a8e8e0b6669fe9dfabce437a282e65e9219
Vladimir Vassilev <vlood.vassilev@gmail.com>
2014-12-25 19:54:12 +0200

    menu order is editable for attachments, readme and translations - updated

--- modify-attachments-meta.php
+++ modify-attachments-meta.php
@@ -14,6 +14,22 @@
  * GitHub Plugin URI: https://github.com/vlood/modify-attachments-meta
  */
 
+/*  Copyright 2014  Vladimir Vassilev  (email : vlood.vassilev@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 if ( ! defined( 'WPINC' ) ) {
         die;
 }
@@ -27,20 +43,20 @@ add_action('plugins_loaded', 'mod_att_meta_init');
 /* Set attachment post date (thanks to http://wordpress.org/support/profile/herrvollbaer, posting a solution here: http://wordpress.org/support/topic/modify-date-of-attachments?replies=5#post-3070220) */
 
 // Add a custom field to an attachment in WordPress
-if(!function_exists('attachment_date_edit')){
-	function attachment_date_edit($form_fields, $post) {
-	    $form_fields['post_date']['label'] = __('Date and time', 'mod-att-meta');
+if(!function_exists('mam_attachment_date_edit')){
+	function mam_attachment_date_edit($form_fields, $post) {
+	    $form_fields['post_date']['label'] = __('Date & time', 'mod-att-meta');
 	    $form_fields['post_date']['value'] = $post->post_date;
 	    $form_fields['post_date']['helps'] = __('Modify the original upload date', 'mod-att-meta');
 	    return $form_fields;
 	}
 	
-	add_filter( 'attachment_fields_to_edit', 'attachment_date_edit', 10, 2);
+	add_filter( 'attachment_fields_to_edit', 'mam_attachment_date_edit', 10, 2);
 }
 
 // save custom field to post_meta
-if(!function_exists('attachment_date_save')){
-	function attachment_date_save($post, $attachment) {
+if(!function_exists('mam_attachment_date_save')){
+	function mam_attachment_date_save($post, $attachment) {
 		if(strtotime($attachment['post_date'])){ //is a valid date?
 			$post['post_date'] = $attachment['post_date'];
 	    	return $post;	
@@ -48,5 +64,27 @@ if(!function_exists('attachment_date_save')){
 			// Do nothing silently. That's what you get when you don't give us valid input!
 		}
 	}
-	add_filter( 'attachment_fields_to_save', 'attachment_date_save', 10, 2);
+	add_filter( 'attachment_fields_to_save', 'mam_attachment_date_save', 10, 2);
+}
+
+
+/* Adding a field for menu_order setting for a file in case you order them by this. Thanks to https://wordpress.org/support/profile/aspacecodyssey for this one: https://wordpress.org/support/topic/adding-menu_order-to-plugin?replies=1 */
+
+if(!function_exists('mam_attachment_menu_order_edit')){
+	function mam_attachment_menu_order_edit($form_fields, $post) {
+	    $form_fields['post_menu_order']['label'] = __('Menu Order', 'mod-att-meta');
+	    $form_fields['post_menu_order']['value'] = $post->menu_order;
+	    $form_fields['post_menu_order']['helps'] = __('Modify the original menu order', 'mod-att-meta');
+	    return $form_fields;
+	}
+
+	add_filter( 'attachment_fields_to_edit', 'mam_attachment_menu_order_edit', 10, 2);
+}
+
+if(!function_exists('mam_attachment_menu_order_save')){
+	function mam_attachment_menu_order_save($post, $attachment) {
+		$post['menu_order'] = $attachment['post_menu_order'];
+    	return $post;
+	}
+	add_filter( 'attachment_fields_to_save', 'mam_attachment_menu_order_save', 10, 2);
 }
--- readme.txt
+++ readme.txt
@@ -2,18 +2,16 @@
 Tags: attachments, modify date, change meta data, edit uploaded files
 Requires at least: 3.8
 Contributors: vloo
-Tested up to: 3.8
-Stable tag: /trunk/
+Tested up to: 4.1
+Stable tag: 0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Allows modification of meta data of attachments, such as date fields (soon to add more, I guess).
+Allows modification of meta data of attachments, such as date fields, menu order... (soon to add more, I guess).
 
 == Description ==
 
-This is the long description.  No limit, and you can use Markdown (as well as in the following sections).
-
-If a user types invalid input, they should be warned. Currently nothing happens - attachment is updated without updating with the wrong data.
+When embedding a file/image/video/something to a page, or you try to edit them in the Library, you'll be allowed to make changes on some meta data. Currently timestamp for uploading of the file and menu order are configurable, although they are still not well validated for wrong input.
 
 = TODO =
 * Metabox like shown here: http://wpsnipp.com/index.php/functions-php/start-date-end-date-metabox-for-events-custom-post-types/;
@@ -21,6 +19,7 @@ If a user types invalid input, they should be warned. Currently nothing happens 
 * Control over which user roles can edit these fields;
 * Validation error messages should be displayed;
 * Use anonymous functions, instead of worrying what'll happen if a function is defined by someone else;
+* If a user types invalid input, they should be warned. Currently nothing happens - attachment is updated without updating with the wrong data.
 
 As I'm lazy as hell, I'll need your help to think of new features to implement!
 
@@ -48,9 +47,16 @@ Nope.
 
 == Screenshots ==
 
-1. How "Edit date and time" looks on the attachment edit page.
+1. How editing date and time looks on the attachment edit page.
 
 == Changelog ==
+
+= 0.2. - 2014.12.25 (Merry Christmas!) =
+
+* Now menu order is editable (thanks to aspacecodyssey);
+* Bulgarian translation was updated;
+* Confirmed WordPress 4.1 compatibility;
+
 = 0.1 =
 
 * Editing an attachment date and time record is now possible;
